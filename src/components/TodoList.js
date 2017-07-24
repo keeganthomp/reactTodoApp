4b79972d9b732b88cab8e69e49c6471db75f39d2
keeganthomp <whereiskeegan@gmail.com>
2017-07-24 15:43:05 -0500

    adding main funcitonality of todo ap

---
+++ src/components/TodoList.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+import TodoItem from "./TodoItem";
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoItem: "",
+      todoList: []
+    };
+
+    // this.getTodoContent = this.getTodoContent.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  getTodoContent = e => {
+    let input = e.target.value;
+    this.setState({ todoItem: input });
+    console.log(this.state.todoItem);
+  };
+
+  handleSubmit = e => {
+    let todoArr = this.state.todoList;
+    todoArr.push(this.state.todoItem);
+    this.setState({todoList: todoArr})
+    this.setState({todoItem: ""});
+    let todoInput = document.querySelector(".todoInput");
+    todoInput.value = "";
+    console.log(this.state.todoList);
+  };
+
+  render() {
+    return (
+      <div className="todoWrapper">
+        <input className="todoInput" type="text" onKeyUp={this.getTodoContent} />
+        <input type="submit" onClick={this.handleSubmit} />
+        <div className="todoList">
+          <TodoItem />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
