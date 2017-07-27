56bf8c4d839c34ec50c7676d4ef189fa32b4dc94
keeganthomp <whereiskeegan@gmail.com>
2017-07-27 16:33:03 -0500

    adding editing capabilities

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,27 +1,77 @@
 import React, { Component } from "react";
 import "../styles/App.css";
 
-
-
 class TodoItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: undefined,
+      todo: {}
+    };
+  }
+
+  editTodo = e => {
+    let index = e.target.id;
+    console.log("index ", index);
+    let todoList = this.props.todos;
+    let item = todoList[index];
+    item.isBeingEdited = true;
+    this.setState({ index });
+    this.setState({ todoBeingEdited: item });
+    // console.log("todoBeingEdited:", this.state.todoBeingEdited);
+  };
+
+  getUpdateTodoContent = e => {
+    let updateTodoValue = e.target.value;
+    this.setState({ todo: {title: updateTodoValue, isBeingEdited: true }});
+  };
+
+  updateTodo = e => {
+    this.props.todos[this.state.index] = this.state.todo;
+    this.props.todos[this.state.index].isBeingEdited = false;
+    
+    console.log(this.props.todos)
+  };
+
   render() {
-    let todos = this.props.todos.map((e, i)=> {
-      return (
-        <div key={Math.random()} className="todoItem">
-          {e}
-          <button id="{i}" className="deleteBtn slide" onClick={this.props.delete}><span className="fa fa-trash"></span></button>
-        </div>
-      );  
+    let todos = this.props.todos.map((e, i) => {
+      if (e.isBeingEdited === false) {
+        return (
+          <div key={Math.random()} className="todoItem">
+            {e.title}
+            <button
+              id={i}
+              className="deleteBtn slide"
+              onClick={this.props.delete}
+            >
+              <span className="fa fa-trash" />
+            </button>
+            <button id={i} onClick={this.editTodo} className="editButton">
+              Edit
+            </button>
+          </div>
+        );
+      } else {
+        return (
+          <div key={Math.random()}>
+            <input
+              onKeyUp={this.getUpdateTodoContent}
+              className="updateInput"
+              type="text"
+            />
+            <button id={i} onClick={this.updateTodo} className="editButton">
+              Update
+            </button>
+          </div>
+        );
+      }
     });
     return (
       <div className="todoContainer">
         {todos}
       </div>
-      
     );
   }
 }
-
-
 
 export default TodoItem;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,19 +6,23 @@ class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todoItem: "",
-      todoList: []
+      todoItem: { tite: "", isBeingEdited: false },
+      todoList: [],
+      todoBeingEdited: ""
     };
 
-    // this.getTodoContent = this.getTodoContent.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.getTodoContent = this.getTodoContent.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   getTodoContent = e => {
     let input = e.target.value;
-    this.setState({ todoItem: input });
+    let newItem = { title: input, isBeingEdited: false };
+    this.setState({ todoItem: newItem });
     console.log(this.state.todoItem);
   };
+
+  
 
   handleSubmit = e => {
     let todoArr = this.state.todoList;
@@ -27,15 +31,18 @@ class TodoList extends Component {
     this.setState({ todoItem: "" });
     let todoInput = document.querySelector(".todoInput");
     todoInput.value = "";
-    console.log(this.state.todoList);
+    console.log(todoArr);
   };
 
   deleteTodo = e => {
     let index = e.target.id;
+    console.log("index ", index);
     let todoArr = this.state.todoList;
     todoArr.splice(index, 1);
     this.setState({ todoList: todoArr });
   };
+
+  
 
   render() {
     return (
@@ -44,6 +51,7 @@ class TodoList extends Component {
           <input
             className="todoInput"
             type="text"
+            name="todo"
             onKeyUp={this.getTodoContent}
           />
           <input
@@ -55,9 +63,12 @@ class TodoList extends Component {
         </div>
         <div className="todoList">
           <TodoItem
+            getContent={this.updateTodo}
+            submit={this.handleSubmit}
             todos={this.state.todoList}
             delete={this.deleteTodo}
             edit={this.editTodo}
+            handleUpdateTodo={this.handleUpdateTodo}
           />
         </div>
       </div>
